81fb50a811018f49a5a914f230d935cc5019bc2d
David Hersey <hersey.da@husky.neu.edu>
2018-05-19 21:04:33 -0700

    create user

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,22 +1,95 @@
 import React from 'react';
 import CourseRow from '../components/CourseRow';
+import CourseService from '../services/CourseService';
 
 export default class CourseList
 	extends React.Component {
 		// eslint-disable-next-line
 	constructor() {
         super();
+	    this.courseService = CourseService.instance;
+	    this.state = {courses: []};
+	    this.titleChanged = this.titleChanged.bind(this);
+   		this.createCourse = this.createCourse.bind(this);
+   		this.deleteCourse = this.deleteCourse.bind(this);
     }
+
+    componentDidMount() {
+    	this.findAllCourses();
+    }
+
+    findAllCourses() {
+    	this.courseService
+    		.findAllCourses()
+      		.then((courses) => {
+        		console.log(courses);
+        		this.setState({courses: courses});
+      		});
+  	}
+
+  	renderCourseRows() {
+	    let courses = null;
+
+	    console.log("render coruse rows")
+	    console.log(this.state)
+	    if(this.state) {
+	      	courses = this.state.courses.map(
+	        	function (course) {
+	          		return <CourseRow key={course.id} course={course}/>
+	        }
+	      )
+	    }
+	    return (courses)
+	}
+
+	titleChanged(event) {
+	    this.setState({
+	      course: { title: event.target.value }
+	    });
+	}
+
+	createCourse() {
+		console.log('createCourse');
+
+	    this.courseService
+	       	.createCourse(this.state.course)
+	       	.then(() => { this.findAllCourses(); });
+	}
+
+	deleteCourse(courseId) {
+		console.log('deleteCourse');
+		this.courseService
+       		.deleteCourse(courseId);
+	}
+
+
     render() {
         return (
         	<div>
 	            <h2>Course List</h2>    
 				<table className="table">
-			    	<thead><tr><th>Title</th></tr></thead>
+			    	<thead>
+			    		<tr>
+				    		<th>Title</th>
+				    		<th>Owned by</th>
+				    		<th>Last Modified</th>
+			    		</tr>
+			    		<tr>
+						   	<th>
+						   		<input onChange={this.titleChanged}
+                         		className="form-control col-lg-8" id="titleFld"
+					        	placeholder="cs101"/>
+					        </th>
+						   	<th>
+						   		<input className="form-control col-lg-5" id="ownerFld" 
+						   		placeholder="owner"/>
+					        </th>
+						   	<th><button onClick={this.createCourse} 
+						   		className="float-right btn btn-primary" type="button">Add</button></th>
+						</tr>
+			    	</thead>
 			    	<tbody>
-			    		<CourseRow/>
-			    		<CourseRow/>
-			    		<CourseRow/>
+			    		{this.renderCourseRows()}
 			    	</tbody>
 				</table>
 			</div>
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import CourseCard from '../components/CourseCard';
-import ModuleList from  './ModuleList';
+//import CourseCard from '../components/CourseCard';
+//import ModuleList from  './ModuleList';
 import CourseList from './CourseList';
 
 export default class CourseManager
@@ -10,21 +10,5 @@ extends Component {
     	<div className="container-fluid">
         	<h1>Course Manager</h1>
         	<CourseList/>
-        	<div>
-        	</div>
-			<ModuleList/>
-        	<div className="row">
-		        <div className="col card-deck">
-					<CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-				    <CourseCard/>
-		 		</div>
-		 	</div>
 	    </div>
 )}}
